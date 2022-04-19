58c742d6a9e4ebfc280b5d09e9f8d386ede180bd
Diffeomorph <64492048+Diffeomorph@users.noreply.github.com>
2022-04-19 10:33:12 +0100

    start multiprocessing

--- webcrawler.py
+++ webcrawler.py
@@ -5,6 +5,7 @@ import requests
 import requests.exceptions
 import collections
 from urllib.parse import urlsplit
+import multiprocessing
 
 class WebCrawler():
     
@@ -15,43 +16,57 @@ class WebCrawler():
         self.external_urls = set()
         self.broken_urls = set()
         
+    def find_all_links(self, cur_url):
+        found_urls = []
+        try:
+            response = requests.get(cur_url)
+        except:
+            self.broken_urls.add(cur_url)
+        
+        parts = urlsplit(cur_url)
+        base = "{0.netloc}".format(parts)
+        strip_base = base.replace("www.", "")
+        base_url = "{0.scheme}://{0.netloc}".format(parts)
+        path = cur_url[:cur_url.rfind('/')+1] if '/' in parts.path else cur_url
+        
+        soup = BeautifulSoup(response.text, "lxml")
+        
+        for link in soup.find_all('a'):
+            anchor = link.attrs["href"] if "href" in link.attrs else ''
+
+            if anchor.startswith('/'):
+                local_link = base_url + anchor
+                self.internal_urls.add(local_link)
+                found_urls.add(local_link)
+            elif not anchor.startswith('http'):
+                local_link = path + anchor
+                self.internal_urls.add(local_link)
+                found_urls.add(local_link)
+            elif strip_base in anchor[:30]: #must be found at start of url
+                self.internal_urls.add(anchor)
+                found_urls.add(anchor)
+            
+            else:
+                self.external_urls.add(anchor)
+                
+        return found_urls
+        
     def crawl(self):
         while self.waiting_urls:
             cur_url = self.waiting_urls.popleft()
             self.visited_urls.add(cur_url)
-            
-            try:
-                response = requests.get(cur_url)
-            except:
-                self.broken_urls.add(cur_url)
-                continue
-            
-            parts = urlsplit(cur_url)
-            base = "{0.netloc}".format(parts)
-            strip_base = base.replace("www.", "")
-            base_url = "{0.scheme}://{0.netloc}".format(parts)
-            path = cur_url[:cur_url.rfind('/')+1] if '/' in parts.path else cur_url
-            
-            soup = BeautifulSoup(response.text, "lxml")
-            
             new_urls = set()
-            for link in soup.find_all('a'):
-                anchor = link.attrs["href"] if "href" in link.attrs else ''
-
-                if anchor.startswith('/'):
-                    local_link = base_url + anchor
-                    self.internal_urls.add(local_link)
-                    new_urls.add(local_link)
-                elif not anchor.startswith('http'):
-                    local_link = path + anchor
-                    self.internal_urls.add(local_link)
-                    new_urls.add(local_link)
-                elif strip_base in anchor[:30]: #must be found at start of url
-                    self.internal_urls.add(anchor)
-                    new_urls.add(anchor)
-                
-                else:
-                    self.external_urls.add(anchor)
+            
+            process_list = []
+            for i in range(6): #cores
+                p =  multiprocessing.Process(target= self.find_all_links)
+                p.start()
+                process_list.append(p)
+            
+            for process in process_list:
+                process.join()
+            
+            print(process_list)
                     
             print(new_urls)
             print("------------------")
